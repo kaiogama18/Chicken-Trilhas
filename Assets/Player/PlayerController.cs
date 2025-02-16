4f2b4879c24a230f0e846731984369bf2195b6b0
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2025-02-16 05:23:29 -0400

    [add] - modulo 03

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     PlayerInput inputActions;
-    // Start is called before the first frame update
 
     public float speed = 2.7f;
+    public float jumpForce = 17f;
+
+    public GameObject shotPrefab;
+    public float shotForce = 10f;
+
+
+    bool canJump = true;
+    bool canAttack = true;
 
     SpriteRenderer sprite;
     Animator animator;
+    Rigidbody2D body;
 
     private void Awake()
     {
@@ -20,6 +28,7 @@ public class PlayerController : MonoBehaviour
     {
         sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
@@ -45,5 +54,52 @@ public class PlayerController : MonoBehaviour
         {
             sprite.flipX = moveInputs.x < 0;
         }
+
+        canJump = Mathf.Abs(body.velocity.y) <= 0.001f;
+        HandlerJumpAction();
+
+        HandlerAttack();
+
+    }
+
+    void HandlerJumpAction()
+    {
+        var jumpPressed = inputActions.Player_Map.Jump.IsPressed();
+
+        if(canJump && jumpPressed)
+        {
+            body.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+
+    void HandlerAttack()
+    {
+        var attackPressed = inputActions.Player_Map.Attack.IsPressed();
+
+        if (canAttack && attackPressed)
+        {
+            canAttack = false;
+
+            animator.SetTrigger("t_attack");
+        }
+
+    }
+
+    public void ShotNewEgg()
+    {
+        var newShot = GameObject.Instantiate(shotPrefab);
+        newShot.transform.position = transform.position;
+
+        var isLookRight = !sprite.flipX;
+        Vector2 shotDirection = shotForce * new Vector2(isLookRight ? -1 : 1, 0);
+
+        newShot.GetComponent<Rigidbody2D>().AddForce(shotDirection, ForceMode2D.Impulse);
+        newShot.GetComponent<SpriteRenderer>().flipY = !isLookRight;
+    }
+
+    public void SetCanAttack()
+    {
+        canAttack = true;
     }
 }
+
